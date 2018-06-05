c3f1696eb24de717216bef8fb2916879d7a1e9ec
贺诗淳 <anhedoniahe@gmail.com>
2018-06-05 23:51:53 +0800

    bug_ can't import app

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,5 @@
-from flask.ext.wtf import Forms
-from wtfforms import StringField, BooleanField
-from wtfforms.validators import DataRequired
+from wtforms import StringField, BooleanField, Form
+from wtforms.validators import DataRequired
 
 class LoginForm(Form):
     openid = StringField('openid', validators = [DataRequired()]) #DataRequired 验证器只是简单地检查相应域提交的数据是否是空。
