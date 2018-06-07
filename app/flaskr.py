b1a88994e7cbfa09c37703b9865dd299d67472c9
贺诗淳 <anhedoniahe@gmail.com>
2018-06-07 15:18:58 +0800

    完成了官方手册上的教程，并添加了测试文件

--- app/flaskr.py
+++ app/flaskr.py
@@ -1,10 +1,39 @@
-from app import app
+import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
-from .forms import LoginForm
+from contextlib import closing
+
+#configuration
+DATABASE = '/tmp/flaskr.db'
+DEBUG = True
+SECRET_KEY = 'development'
+USERNAME = 'admin'
+PASSWORD = 'lsof04Mt'
+
+#create a little application
+app = Flask(__name__)
+app.config.from_object(__name__)
+# from_object() 将会寻找给定的对象(如果它是一个字符串，则会导入它)， 搜寻里面定义的全部大写的变量。在我们的这种情况中，配置文件就是我们上面写的几行代码。 你也可以将他们分别存储到多个文件。
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+    g.db.close()
 
 @app.route('/')
 def show_entries():
-    cur = g.db.excute('select title, text from entries order by id desc')
+    cur = g.db.execute('select title, text from entries order by id desc')
     entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
     return render_template('show_entries.html', entries = entries)
 
@@ -17,24 +46,6 @@ def add_entry():
     g.db.commit()
     flash('New entry was successfully posted')
     return redirect(url_for('show_entries'))
-
-# @app.route('/index')
-# def index():
-#     user = { 'nickname': 'Miguel' } # fake user
-#     posts = [ # fake array of posts
-#         {
-#             'author': { 'nickname': 'John' },
-#             'body': 'Beautiful day in Portland!'
-#         },
-#         {
-#             'author': { 'nickname': 'Susan' },
-#             'body': 'The Avengers movie was so cool!'
-#         }
-#     ]
-#     return render_template("index.html",
-#         title = 'Home',
-#         user = user,
-#         posts = posts)
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
@@ -55,3 +66,6 @@ def logout():
     session.pop('logged_in', None)
     flash('You were logged out')
     return redirect(url_for('show_entries'))
+
+if __name__ == '__main__':
+    app.run()
