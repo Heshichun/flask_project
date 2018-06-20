9c358ab8bd6a37e89f1a42381aee663c4abe36c9
贺诗淳 <anhedoniahe@gmail.com>
2018-06-20 09:44:36 +0800

    使用 pycharm 更新了一下配置

--- app/__init__.py
+++ app/__init__.py
@@ -3,13 +3,15 @@ import sqlite3
 from config import config
 from flask import Blueprint
 
+
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
+    
     from main.admin import admin
     app.register_blueprint(admin)
 
     return app
 
-#__init__.py里面创建实例,应用实例对象创建完再引入视图函数的模块,因为这时候视图函数上的@app.route()才有效
+    # __init__.py里面创建实例,应用实例对象创建完再引入视图函数的模块,因为这时候视图函数上的@app.route()才有效
--- app/route/admin.py
+++ app/route/admin.py
@@ -1,16 +1,19 @@
-from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash,Blueprint
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash, Blueprint
 from forms import LoginForm
 from . import admin
+
 
 @admin.route('/')
 def index():
     return '<h1>Hello World!</h1>'
 
+
 @admin.route('/1')
 def index2():
     return redirect(url_for('.index'))
 
-@admin.route('/login', methods = ['GET', 'POST'])
+
+@admin.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
@@ -24,17 +27,19 @@ def login():
             return redirect(url_for('.index'))
     return render_template('login.html', error=error)
 
+
 @admin.route('/logout')
 def logout():
     session.pop('logged_in', None)
     flash('You were logged out')
     return redirect(url_for('admin.index'))
 
-@admin.route('/add', methods = ['POST'])
+
+@admin.route('/add', methods=['POST'])
 def add_entry():
     if not session.get('logged_in'):
         abort(401)
-    g.db.excute('insert into entries (title, text) values (?, ?)',[request.form['title'],request.form['text']])
+    g.db.excute('insert into entries (title, text) values (?, ?)', [request.form['title'], request.form['text']])
 
     g.db.commit()
     flash('New entry was successfully posted')
